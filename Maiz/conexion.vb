38cc22084fab754ce469f9ba10d2558d0bfcea4a
mikeCq <miguelcarrilloqnz@hotmail.com>
2016-11-12 10:03:46 -0700

    chingo de cambios en muchas cosas

--- Maiz/conexion.vb
+++ Maiz/conexion.vb
@@ -8,8 +8,9 @@ Module conexion
 
     Sub abrir()
         Try
-            cnn = New SqlConnection("Data Source=MSISTEMAS;Initial Catalog=MAIZ;Persist Security Info=True;User ID=sa;Password=Usuario01") '*****  CONEXION MSISTEMAS
+            'cnn = New SqlConnection("Data Source=MSISTEMAS;Initial Catalog=MAIZ;Persist Security Info=True;User ID=sa;Password=Usuario01") '*****  CONEXION MSISTEMAS
             'cnn = New SqlConnection("Data Source=SERVIDOR;Initial Catalog=TRIGO_1;User ID=UserTrigo;Password=usuario1") '***** conexion CONEXCION SERVIDOR
+            cnn = New SqlConnection("Data Source=SERVER2008\SERVER12;Initial Catalog=MAIZ;Persist Security Info=True;User ID=sa;Password=Usuario01") '***** CONEXION A SERVIDOR PRINCIPAL 
             cnn.Open()
         Catch ex As Exception
             MsgBox("No se pudo conectar" + ex.ToString)
