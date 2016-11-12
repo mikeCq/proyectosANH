38c8b79ce286ebb3ae6595c9ae2e1e1119f43e6b
mikeCq <miguelcarrilloqnz@hotmail.com>
2016-11-11 21:59:28 -0700

    aasd

--- Maiz/liquidacionCalculos.vb
+++ Maiz/liquidacionCalculos.vb
@@ -661,7 +661,7 @@ Public Class liquidacionCalculosProd
     End Sub
 
     Private Sub ContratoOLibre()
-        If RbSi.Checked = True And RbContrato.Checked = True Or RbSi.Checked = True And RbLibre.Checked = True Then 'prueba
+        If RbSi.Checked = True And RbContrato.Checked = True Or RbSi.Checked = True And RbLibre.Checked = True Then 'prueba nueva
             If RbContrato.Checked = False And RbLibre.Checked = True Then
                 TxTipoCambio.Enabled = False
                 TxTipoCambio.Text = ""
