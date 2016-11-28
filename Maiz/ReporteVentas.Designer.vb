5e0d8e34e140a2687fd4caa71316ef5c4b0a8d10
mikeCq <miguelcarrilloqnz@hotmail.com>
2016-11-28 16:53:34 -0700

    Cambios en RptVenta y Form ReporteVentas

--- Maiz/ReporteVentas.Designer.vb
+++ Maiz/ReporteVentas.Designer.vb
@@ -22,17 +22,38 @@ Partial Class ReporteVentas
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.CrLiquidacionVenta = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
         Me.SuspendLayout()
+        '
+        'CrLiquidacionVenta
+        '
+        Me.CrLiquidacionVenta.ActiveViewIndex = -1
+        Me.CrLiquidacionVenta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CrLiquidacionVenta.Cursor = System.Windows.Forms.Cursors.Default
+        Me.CrLiquidacionVenta.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CrLiquidacionVenta.Location = New System.Drawing.Point(0, 0)
+        Me.CrLiquidacionVenta.Name = "CrLiquidacionVenta"
+        Me.CrLiquidacionVenta.ShowCloseButton = False
+        Me.CrLiquidacionVenta.ShowGroupTreeButton = False
+        Me.CrLiquidacionVenta.ShowLogo = False
+        Me.CrLiquidacionVenta.ShowParameterPanelButton = False
+        Me.CrLiquidacionVenta.ShowRefreshButton = False
+        Me.CrLiquidacionVenta.Size = New System.Drawing.Size(877, 559)
+        Me.CrLiquidacionVenta.TabIndex = 2
+        Me.CrLiquidacionVenta.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None
         '
         'ReporteVentas
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(612, 457)
+        Me.ClientSize = New System.Drawing.Size(877, 559)
+        Me.Controls.Add(Me.CrLiquidacionVenta)
         Me.Name = "ReporteVentas"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "ReporteVentas"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents CrLiquidacionVenta As CrystalDecisions.Windows.Forms.CrystalReportViewer
 End Class
